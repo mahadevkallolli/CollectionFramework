778ef4933a8db9bc4cd549a0532c4e34325dbead
mahadev kallolli <mahadevk282@gmail.com>
2023-09-29 17:03:06 +0530

    class not found exception

--- src/main/java/org/collectionframeworkdemo/Main.java
+++ src/main/java/org/collectionframeworkdemo/Main.java
@@ -1,5 +1,18 @@
-package org.collectionframeworkdemo;public class Main {
+package org.collectionframeworkdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> list= new ArrayList<>();
+        list.add(0,100);
+        list.add(100);
+        list.add(2,200);
+
+        for(Integer lists: list){
+            System.out.println(lists);
+        }
     }
 }
